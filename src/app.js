42155f83867aec9c729081ead6e460c53d498991
Luis Guilarte Rodriguez <147260322+Luisgr10@users.noreply.github.com>
2024-01-20 12:03:55 +0000

    Actualizacion proyecto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function randomCard() {
+    let suits = [
+      document.getElementById("heart").innerHTML,
+      document.getElementById("club").innerHTML,
+      document.getElementById("diamond").innerHTML,
+      document.getElementById("spade").innerHTML
+    ];
+    let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+    let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+    document.getElementById("suitsTop").innerHTML = randomSuit;
+    document.getElementById("cardNumber").innerHTML = randomNumber;
+    document.getElementById("suitsBtn").innerHTML = randomSuit;
+  }
+
+  randomCard();
+
+  let generateCard = document.getElementById("generateCard");
+  generateCard.addEventListener("click", randomCard);
 };
